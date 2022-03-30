5b8b93dadffc9bb6498d6eb2daefccc22077dd74
Alexsandro Ryan <alexsandrocosta855@gmail.com>
2022-03-30 11:25:36 -0300

    Commit 30.03

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -7,9 +7,12 @@ app.get('/hello', (req, res)=> {
     res.send('Task Manager App')
 })
 
+app.use(express.json());
+
 app.use('/api/v1/tasks', tasks)
 
-// app.get('/api/v1/tasks')    .       - get all the tasks
+
+// app.get('/api/v1/tasks')           - get all the tasks
 // app.post('/api/v1/tasks')          - create a new task
 // app.get('/api/v1/tasks/:id')       - get single task
 // app.patch('/api/v1/tasks/:id')     - update task
